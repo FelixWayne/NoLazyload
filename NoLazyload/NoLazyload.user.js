084349246b1f7a288ea6aea924283b528d1f2f4b
ivysrono <ivysrono@users.noreply.github.com>
2018-07-15 20:16:23 +0800

    1.2
    修复修改标题功能

--- NoLazyload/NoLazyload.user.js
+++ NoLazyload/NoLazyload.user.js
@@ -3,13 +3,10 @@
 // @name:zh-CN    原图显示微信公众号文章
 // @namespace     https://github.com/ivysrono/Scripts
 // @author        ivysrono
-// @version       1.1
+// @version       1.2
 // @match         https://mp.weixin.qq.com/s*
 // @grant         none
 // ==/UserScript==
-
-// 将网页标题修改为文章标题
-document.title = document.getElementById("activity-name").innerHTML;
 
 // 去除延迟加载，直接载入原图。
 var imgs = document.getElementsByClassName("img_loading");
@@ -23,3 +20,15 @@ for (i = 0; i < length; i++) {
     imgs[i].style.height = "";
   }
 }
+
+// 将网页标题修改回文章标题
+var runcount = 0;
+var realTitle = document.getElementById("activity-name").innerHTML;
+function real() {
+  // “微信扫一扫关注该公众号”出现后会将标题修改为公众号名，可能脚本执行后又被改回去，二次修改可以保证成功。也许用 MutationObserver 是更好的选择。
+  if (document.title != realTitle && runcount < 2) {
+    document.title = realTitle;
+    runcount += 1;
+  }
+}
+setInterval(real, 1000);
