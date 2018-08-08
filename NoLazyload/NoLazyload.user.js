2f2f50bfc6075a2acd62977e1f311bbd816b4acf
ivysrono <ivysrono@users.noreply.github.com>
2018-08-08 22:14:37 +0800

    1.3
    兼容全文字状况；
    用 for of 等 ES6 语句改写。

--- NoLazyload/NoLazyload.user.js
+++ NoLazyload/NoLazyload.user.js
@@ -1,34 +1,32 @@
 // ==UserScript==
-// @name          Real wechat subscription articles images
-// @name:zh-CN    原图显示微信公众号文章
+// @name          NoLazyload
 // @namespace     https://github.com/ivysrono/Scripts
 // @author        ivysrono
-// @version       1.2
+// @version       1.3
 // @match         https://mp.weixin.qq.com/s*
 // @grant         none
 // ==/UserScript==
 
 // 去除延迟加载，直接载入原图。
-var imgs = document.getElementsByClassName("img_loading");
-var length = imgs.length
-for (i = 0; i < length; i++) {
-  var realSrc = imgs[i].dataset.src;
-  if (realSrc) {
-    imgs[i].src = realSrc;
+let imgs = document.getElementsByClassName("img_loading");
+if (imgs) {
+  for (let img of imgs) {
+    let realSrc = img.dataset.src;
+    img.src = realSrc;
     // 对付垃圾公众号的长宽限制 https://mp.weixin.qq.com/s/b4V3eaFYYI0h90vIVEqKzw
-    imgs[i].style.width = "";
-    imgs[i].style.height = "";
+    img.style.width = "";
+    img.style.height = "";
   }
 }
 
 // 将网页标题修改回文章标题
-var runcount = 0;
-var realTitle = document.getElementById("activity-name").innerHTML;
+let runcount = 0;
+let realTitle = document.getElementById("activity-name").innerHTML;
 function real() {
   // “微信扫一扫关注该公众号”出现后会将标题修改为公众号名，可能脚本执行后又被改回去，二次修改可以保证成功。也许用 MutationObserver 是更好的选择。
   if (document.title != realTitle && runcount < 2) {
     document.title = realTitle;
-    runcount += 1;
+    runcount++;
   }
 }
 setInterval(real, 1000);
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 当前第一版仅支持微信公众号页面。
 
-[userScript](https://github.com/ivysrono/NoLazyload/raw/master/NoLazyload/NoLazyload.user.js) 版本在 Firefox for Android + Violentmonkey 下无效，故打包为扩展。PC 用户完全可以使用任意脚本管理器安装。
+[userScript](https://github.com/ivysrono/NoLazyload/raw/master/NoLazyload/NoLazyload.user.js) 版本在 Firefox + Violentmonkey 下无效，故打包为扩展。PC 用户完全可以使用任意脚本管理器安装。
 
 综上，暂无上架 ChromeWebStore 的计划。
