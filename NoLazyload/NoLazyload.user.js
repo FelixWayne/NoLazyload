7322cb66e958af7aafa7fc98a2159d9985e3594a
ivysrono <ivysrono@users.noreply.github.com>
2018-09-01 17:47:53 +0800

    1.6
    改进稳定性，保证生效，轻微牺牲性能。

--- NoLazyload/NoLazyload.user.js
+++ NoLazyload/NoLazyload.user.js
@@ -2,38 +2,40 @@
 // @name          NoLazyload
 // @namespace     https://github.com/ivysrono/Scripts
 // @author        ivysrono
-// @version       1.5
+// @version       1.6
 // @match         https://mp.weixin.qq.com/s*
 // @grant         none
 // ==/UserScript==
 
-// 去除延迟加载，直接载入原图。
-let imgs = document.getElementsByTagName("img");
-if (imgs) { // 可能存在无图页面
-  for (let img of imgs) {
-    let realSrc = img.dataset.src;
-    if (realSrc && img.src !== realSrc){ // 可能没有延迟加载图或原图已经载入
-      img.src = realSrc;
-      // 对付垃圾公众号的长宽限制 https://mp.weixin.qq.com/s/b4V3eaFYYI0h90vIVEqKzw
-      img.style.width = "";
-      img.style.height = "";
-    }
-  }
-}
-
-// 将网页标题修改回文章标题
 let runcount = 0;
 let realTitle = document.getElementById("activity-name").innerHTML;
+let imgs = document.getElementsByTagName("img");
+
 function real() {
-  // “微信扫一扫关注该公众号”出现后会将标题修改为公众号名，可能脚本执行后又被改回去，二次修改可以保证成功。https://mp.weixin.qq.com/s/cyFN8mEq00dgpklQn6woeg
-  // 也许用 MutationObserver 是更好的选择。
-  if (runcount >= 2) {
-    clearInterval(change);
+  // 去除图片延迟加载
+  if (imgs.length > 0) { // 可能存在无图页面
+    for (let img of imgs) {
+      let realSrc = img.dataset.src;
+      if (realSrc && img.src !== realSrc) { // 可能没有延迟加载图或原图已经载入
+        img.src = realSrc;
+        // 对付垃圾公众号的长宽限制 https://mp.weixin.qq.com/s/b4V3eaFYYI0h90vIVEqKzw
+        img.style.width = "";
+        img.style.height = "";
+      }
+    }
   }
+  /*
+  将网页标题修改回文章标题。
+  “微信扫一扫关注该公众号”出现后会将标题修改为公众号名，可能脚本执行后又被改回去，二次修改可以保证成功。https://mp.weixin.qq.com/s/cyFN8mEq00dgpklQn6woeg
+  也许用 MutationObserver 是更好的选择。
+  */
   if (document.title != realTitle && runcount < 2) {
     document.title = realTitle;
     runcount++;
     console.log('runcount=' + runcount)
+  }
+  if (runcount >= 2) {
+    clearInterval(change);
   }
 }
 change = setInterval(real, 1000);
--- NoLazyload/README.md
+++ NoLazyload/README.md
@@ -1,5 +1,7 @@
-由于 Violentmonkey 迟迟不能解决 [Firefox CSP issue](https://github.com/violentmonkey/violentmonkey/issues/408)，为此专门安装 GM 又太麻烦，故将[脚本](https://github.com/ivysrono/NoLazyload/raw/master/NoLazyload/NoLazyload.user.js)打包成扩展。目前第一版仅支持微信公众号文章页面。
+由于 Violentmonkey 迟迟不能解决 [Firefox CSP issue](https://github.com/violentmonkey/violentmonkey/issues/408)，故将[脚本](https://github.com/ivysrono/NoLazyload/raw/master/NoLazyload/NoLazyload.user.js)打包成扩展。目前仅支持微信公众号文章页面。
 
 Chrome 用户可以用任意脚本管理器[安装](https://github.com/ivysrono/NoLazyload/raw/master/NoLazyload/NoLazyload.user.js)。
+Firefox + Greasemonkey/Tampermonkey 应当都可以正常使用。
+其实 Firefox 用户也可以继续使用 Violentmonkey 安装管理本脚本，但需要加装 Greasemonkey。至于为什么空白的 GM 就能解决问题，还请大佬指点。
 
 前辈 [LightTerminal/nolazyload](https://github.com/LightTerminal/nolazyload) 提供了适用于全网的去除延迟加载脚本，但会造成很多误伤，建议一个一个需要的网站进行处理。
